ea6b0d5b73618c4d6b493e0d3846e1d042dfa810
zaissov <zaissov@yahoo.com>
2021-05-26 14:28:01 +0100

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Ola</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
